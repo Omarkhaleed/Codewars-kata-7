f6c16e5b0820e012f9249da9f605b897449de009
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-07-14 21:55:07 +0200

    Create 09-Growth of a Population.cpp

---
+++ 09-Growth of a Population.cpp
@@ -0,0 +1,15 @@
+class Arge
+{
+  public:
+  static int nbYear(int p0, double percent, int aug, int p)
+  {
+    int count=0;
+    while(p>p0){
+      p0=p0+(p0*(percent/100))+aug;
+      count++;
+    }
+    return count;
+    
+    
+  }
+};
