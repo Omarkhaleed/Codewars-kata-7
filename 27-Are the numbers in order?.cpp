e653d55d5bb1b2fee5c3d1089656053b34e4fa1b
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-07-14 22:16:28 +0200

    Create 27-Are the numbers in order?.cpp

---
+++ 27-Are the numbers in order?.cpp
@@ -0,0 +1,14 @@
+bool isAscOrder(std::vector<int> arr)
+{
+  if(is_sorted(arr.begin(),arr.end()))
+  return true;
+  else
+  return false;
+  }
+ /*for(int i=1;i<arr.size();i++){
+  if(arr[i]<arr[i-1])
+  return false;
+  else
+  return true;
+  */
+  
