3e797d130a906d5142623f2a600f20a75c47f412
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-07-14 22:05:57 +0200

    Create 23-Sort Numbers.cpp

---
+++ 23-Sort Numbers.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+std::vector<int> solution(std::vector<int> nums) {
+  std::vector<int>v=nums;
+  if(v.size()==0)
+    return v;
+  sort(v.begin(),v.end());
+  for (auto x : v)
+        return v;
+  }
